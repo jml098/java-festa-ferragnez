6b783b5baaa07aa9b2c7c683636b864b7c46ae7b
Jean <jmlrissac@gmail.com>
2023-07-13 14:20:51 +0200

    Code formatting and made scanner final.

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 //Consegna: creare un progetto java-festa-ferragnez con il package com.ferragnez.party,
 // a cui aggiungere la classe CheckGuest
 
+
 //Nel programma bisogna:
 //creare e inizializzare l’array contenente i nomi degli invitati
 //chiedere all’utente come si chiama
@@ -15,12 +16,14 @@ import java.util.Scanner;
 // lasciarlo entrare o rispedirlo cortesemente da dove è venuto
 // se avete usato il ciclo for per cercare il nome nella lista nella consegna base,
 // implementare lo stesso programma usando il ciclo while, e viceversa
-public class CheckGuestBonus {
-    private static Scanner scanner = new Scanner(System.in);
+public class CheckGuest {
+
+    private static final Scanner scanner = new Scanner(System.in);
     private static String[] guestsList;
 
+
     public static void main(String[] args) {
-        guestsList = new String[]{
+        guestsList = new String[] {
                 "Dua Lipa",
                 "Paris Hilton",
                 "Manuel Agnelli",
@@ -33,10 +36,12 @@ public class CheckGuestBonus {
                 "Rachel Zeilic"
         };
 
-        System.out.println("Scegli quale metodo utilizzare: \n" +
-                "-ForLoop (0)\n" +
-                "-WhileLoopVersione1 (1)\n" +
-                "-WhileLoopVersione2 (2)");
+        System.out.println("""
+                Scegli quale metodo utilizzare:\s
+                -ForLoop (0)
+                -WhileLoopVersione1 (1)
+                -WhileLoopVersione2 (2)""");
+
         switch (Integer.parseInt(scanner.nextLine())) {
             case 0 -> checkWithFor();
             case 1 -> checkWithWhileVersion1();
@@ -44,6 +49,7 @@ public class CheckGuestBonus {
             default -> System.out.println("Devi scegliere tra 0, 1 o 2");
         }
     }
+
 
     private static void checkWithFor() {
 
@@ -70,6 +76,8 @@ public class CheckGuestBonus {
         }
 
     }
+
+
     private static void checkWithWhileVersion1() {
 
         String name;
@@ -79,12 +87,14 @@ public class CheckGuestBonus {
         name = scanner.nextLine().toLowerCase();
 
         i = 0;
-        while (i < guestsList.length )  {
+        while (i < guestsList.length) {
             String guest = guestsList[i];
 
             if (guest.toLowerCase().equals(name)) {
                 break;
-            } else i++;
+            } else {
+                i++;
+            }
         }
 
         if (i >= guestsList.length) {
@@ -94,6 +104,8 @@ public class CheckGuestBonus {
         }
 
     }
+
+
     private static void checkWithWhileVersion2() {
 
         String name;
@@ -103,7 +115,9 @@ public class CheckGuestBonus {
         name = scanner.nextLine().toLowerCase();
 
         i = 0;
-        while (i < guestsList.length && !guestsList[i].toLowerCase().equals(name))  i++;
+        while (i < guestsList.length && !guestsList[i].toLowerCase().equals(name)) {
+            i++;
+        }
 
         if (i >= guestsList.length) {
             System.out.println("Ci spiace ma non sei nella lista degli invitati.");
